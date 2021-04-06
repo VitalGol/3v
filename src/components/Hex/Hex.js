c2a8c3ba095c878e262e1ae76b7c4dfaaa89fcbd
Vitaly <vitalgolob@gmail.com>
2021-04-06 16:11:49 +0300

    add Hex

---
+++ src/components/Hex/Hex.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Hex.scss";
+const Hex = () => {
+  return (
+    <div className="hexagon">
+      <div class="corner left"></div>
+      <div className="text">
+        <a href="">test</a>
+      </div>
+
+      <div className="corner right"></div>
+    </div>
+  );
+};
+
+export default Hex;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Hex from "../Hex/Hex";
 import "./Home.scss";
 const Home = () => {
   return (
@@ -9,7 +10,24 @@ const Home = () => {
         dolorum blanditiis, similique modi perspiciatis, perferendis voluptas
         totam officia sequi, ea doloremque. Saepe voluptatum voluptates
         explicabo praesentium, ipsum atque.
+        <svg
+          aria-hidden="true"
+          class="svg-icon iconFacebook"
+          width="20"
+          height="20"
+          viewBox="0 0 18 18"
+        >
+          <path
+            d="M3 1a2 2 0 00-2 2v12c0 1.1.9 2 2 2h12a2 2 0 002-2V3a2 2 0 00-2-2H3zm6.55 16v-6.2H7.46V8.4h2.09V6.61c0-2.07 1.26-3.2 3.1-3.2.88 0 1.64.07 1.87.1v2.16h-1.29c-1 0-1.19.48-1.19 1.18V8.4h2.39l-.31 2.42h-2.08V17h-2.5z"
+            fill="#4167B2"
+          ></path>
+        </svg>
       </p>
+      <div className="home-hex">
+        <Hex />
+        <Hex />
+        <Hex />
+      </div>
     </div>
   );
 };
